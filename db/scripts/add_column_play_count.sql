5097482f0a6ae9a7bbcdbdb3e71f737a04ae4050
Yohei Munesada <y.munesada@gmail.com>
2013-10-07 19:44:08 +0900

    add alter table script

---
+++ db/scripts/add_column_play_count.sql
@@ -0,0 +1,5 @@
+alter table music add column play_count numeric;
+alter table music add column play_count_speed numeric;
+alter table music_link add column play_count numeric;
+alter table music_link add column play_count_speed numeric;
+
