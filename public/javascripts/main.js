383753a03a216d52aad9c6d5bea20817d6628da3
Yohei Munesada <y.munesada@gmail.com>
2013-10-09 09:38:36 +0900

    add google analytics

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -48,55 +48,66 @@ require([
         top: function () {
             this.sendAction('/#');
             this.appView.toTop();
+            _gaq.push(['_trackPageview', '/']);
         },
 
         poplist: function (feelingId) {
             this.sendAction('/#poplist/' + feelingId);
             this.appView.toPopList(feelingId);
+            _gaq.push(['_trackPageview', '/#poplist/' + feelingId]);
         },
 
         musicDetail: function (musicId) {
             this.sendAction('/#music/' + musicId);
             this.appView.toMusicDetail(musicId);
+            _gaq.push(['_trackPageview', '/#music/' + musicId]);
         },
 
         defaultRoute: function () {
             this.top();
+            _gaq.push(['_trackPageview', '/']);
         },
 
         musicSearch: function () {
             this.sendAction('/#music/search');
             this.appView.toMusicSearch();
+            _gaq.push(['_trackPageview', '/#music/search']);
         },
 
         addPop: function (musicId) {
             this.sendAction('/#music/' + musicId + '/pop/add');
             this.appView.toAddPop(musicId);
+            _gaq.push(['_trackPageview', '/#music/' + musicId + '/pop/add']);
         },
 
         login: function () {
             this.sendAction('/#login');
             this.appView.toLogin();
+            _gaq.push(['_trackPageview', '/#login']);
         },
 
         mypage: function () {
             this.sendAction('/#mypage');
             this.appView.toMypage();
+            _gaq.push(['_trackPageview', '/#mypage']);
         },
 
         userPage: function (userId) {
             this.sendAction('/#user/' + userId);
             this.appView.toUserPage(userId);
+            _gaq.push(['_trackPageview', '/#user/' + userId]);
         },
 
         registUser: function () {
             this.sendAction('/#user/new');
             this.appView.toRegistUserPage();
+            _gaq.push(['_trackPageview', '/#user/new']);
         },
 
         artist: function (artistId) {
             this.sendAction('/#artist/' + artistId);
             this.appView.toArtist(artistId);
+            _gaq.push(['_trackPageview', '/#artist/' + artistId]);
         },
 
 
