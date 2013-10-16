d9ef3c5640557b6064dca8ee9b0281edf81c5de3
Yohei Munesada <y.munesada@gmail.com>
2013-10-17 00:41:11 +0900

    add colmun

---
+++ db/scripts/add_column_feeling_id.sql
@@ -0,0 +1,2 @@
+alter table music add column feeling_id integer;
+
--- db/scripts/add_column_play_count.sql
+++
@@ -1,5 +0,0 @@
-alter table music add column play_count numeric;
-alter table music add column play_count_speed numeric;
-alter table music_link add column play_count numeric;
-alter table music_link add column play_count_speed numeric;
-
