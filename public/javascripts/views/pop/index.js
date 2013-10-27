db579ad1a62ae8f56bdabc5a4237a7901c086e62
Yohei Munesada <y.munesada@gmail.com>
2013-10-27 23:52:35 +0900

    ポップ追加画面を作成

--- public/javascripts/views/pop/index.js
+++ public/javascripts/views/pop/index.js
@@ -17,42 +17,31 @@ define([
         // data field.
         displayType:    'normal', // normal or modal
         type:           null, // add or update
-        music_id:       null,
-        pop_id:         null,
-        feelingList:    null,
-        popModel:       new PopModel(),
-
-        // functional field. 
-        userStorage: new UserStorage(),
-        
+        musicId:       null,
+        popId:         null,
 
 
         initialize: function () {
 
-            // intialize data.
-            this.feelingList = this.userStorage.getCommon().feelings;
-
-            // bind event.
-            _.bindAll(this, 'render', 'show', 'showMusicInfo', 'showPopInfo', 'refreshFeelingSelect', 'addOrUpdate', 'popSaveSuccess', 'dealloc');
-            this.popModel.bind('change', this.ShowPopInfo);
-            this.popModel.bind('sync', _.bind(this.popSaveSuccess, this));
-        },
-
-
-
-        template: $('#page_pop').html(),
-
-
-
-        events: {
-            'click #updatePopBtn': 'addOrUpdate'
+            // auto event bind.
+            _.bindEvents(this);
         },
 
 
 
         render: function () {
-            this.$el.append(_.template(this.template, {type: this.type}));
 
+            // レンダリング
+            var snipet = _.mbTemplate('page_pop', {
+                type: this.type,
+                feelingList: _.mbStorage.getCommon().feelings,
+                music: this.music,
+                pop: this.pop
+            });
+            this.$el.html(snipet);
+
+
+            // モーダルの場合の表示制御
             if (this.displayType === 'modal') {
 
                 // expand area.
@@ -65,7 +54,7 @@ define([
                 var $blackout = $('<div class="blackout"/>').css({opacity:1});
                 $blackout.on('click', _.bind(function () {
                     this.$el.remove();
-                    this.destory();
+                    this.dealloc();
                 }, this));
                 this.$el.prepend($blackout);
 
@@ -74,58 +63,50 @@ define([
             }
         },
 
-        showMusicInfo: function () {
-            console.log('showMusicInfo');
-            var musicInfo = _.template($('#page_music_detail_info').html(), this.musicModel.attributes);
-            this.$el.find('#musicInfoArea').html(musicInfo);
+
+
+        /**
+            文字数を数える
+        */
+        countCharacters: function () {
+            var length = this.$el.find('#comment').val().length;
+            this.$el.find('#numOfCharacters').text(length);
         },
 
 
-        showPopInfo: function () {
-            console.log('showPopInfo');
-            var popInfo = _.template($('#pop_info').html(), this.popModel.attributes);
-            console.log(popInfo);
-            this.$el.find('#popInfoArea').html(popInfo);
-            console.log(this, this.$el, this.$el.find('#musicInfoArea'));
-            console.log('feelingList: ', this.feelingList);
-            if (this.feelingList.length > 0) {
-                this.refreshFeelingSelect();
-            }
-        },
+        /**
+            POPを投稿する
+        */
+        addPop: function () {
+
+            // 選択されたキモチ
+            var feelingId = this.$el.find('[name="feelingSelect"]:checked').val();
+            // コメント
+            var comment = this.$el.find('#comment').val();
 
 
-        refreshFeelingSelect: function () {
-            console.log('refreshFeelingSelect', this.feelingList.models);
-            var html = _.template($('#pop_page_feeling_list_options').html(), {feelings: this.feelingList, pop: this.popModel.attributes});
-            this.$el.find('#feeling_select').html(html);
-        },
-
-        addOrUpdate: function () {
-            console.log('addOrUpdate');
-            this.popModel.attributes.feeling_id = $('#feeling_select').val();
-            this.popModel.attributes.music_id = this.music_id;
-            this.popModel.attributes.comment = $('[name="comment"]').val();
-
-            // TODO Validate
-
-
-            if (this.type === 'add') {
-                this.popModel.create();
-            } else {
-                this.popModel.update();
+            // キモチが選択されていない場合はだめ
+            if (!feelingId) {
+                alert('ドロップの種類を選択してください');
+                return;
             }
 
-        },
-
-        popSaveSuccess: function () {
-            alert('save successed');
-
-            if (this.displayType === 'modal') {
-                this.$el.remove();
-                this.destory();
+            // コメントが入力されていない場合はだめ
+            if (!comment) {
+                alert('感想は１文字以上入力してください');
             }
 
-            mb.router.navigate('/music/' + this.music_id, true);
+            // 登録する
+            var pop = new PopModel();
+            pop.set('music_id', this.musicId);
+            pop.set('feeling_id', feelingId);
+            pop.set('comment', comment);
+            pop.bind('sync', _.bind(function () {
+                alert('登録完了しました');
+                location.reload();
+            }, this));
+            pop.save();
+
         },
 
 
@@ -136,19 +117,48 @@ define([
 
             // set data.
             this.type = (popId ? 'update' : 'add');
+            this.musicId = musicId;
             this.music_id = musicId;
             this.pop_id = popId;
             this.displayType = displayType;
 
-            // render base.
-            this.render();
 
-            // show Pop.
-            if (popId) {
-                this.popModel.loadData(popId);
-            } else {
-                this.showPopInfo();
-            }
+            // musicのロード
+            this.music = new MusicModel();
+            this.music.set('id', musicId);
+            this.music.bind('sync', _.bind(function () {
+
+                // 新規の場合には、画面をレンダリング
+                if (this.type === 'add') {
+                    this.render();
+                
+                // 変更の場合に既にpopIdがあれば、レンダリング
+                } else if (this.popId) {
+                    this.render();
+                
+                } else {
+                    // popのロード待ち
+                }
+
+            }, this));
+            this.music.fetch();
+
+
+
+            // popのロード
+            this.pop = new PopModel();
+            this.pop.set('id', popId);
+            this.pop.bind('sync', _.bind(function () {
+
+                // 既にMusicがロード済みの場合には、表示
+                if (this.music) {
+                    this.render();
+                
+                } else {
+                    // Musicがまだ無ければそれ待ち
+                }
+
+            }, this));
 
         },
 
