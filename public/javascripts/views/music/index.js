a499caab72956b11024ea183fd2316d27c49ff01
Yohei Munesada <y.munesada@gmail.com>
2013-10-30 20:54:16 +0900

    リンク再生回数通知を実装

--- public/javascripts/views/music/index.js
+++ public/javascripts/views/music/index.js
@@ -209,6 +209,11 @@ define([
             // play
             console.log('play music. arraycount=', options.musicArray.length, ',startPos=', options.startPos);
             mb.musicPlayer.playMusics(options);
+
+
+            // リンクの再生回数を通知
+            var musicLinkId = $(e.currentTarget).parents('[data-musiclink-id]').data('musiclink-id');
+            _.addMusicLinkPlayCount(this.music.id, musicLinkId);
         },
 
 
