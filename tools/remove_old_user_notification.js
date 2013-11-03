d71d89757556bf2314817e2a653b769c3bb770ce
Yohei Munesada <y.munesada@gmail.com>
2013-11-03 23:23:12 +0900

    古いお知らせを削除するバッチを作成

--- tools/remove_old_user_notification.js
+++ tools/remove_old_user_notification.js
@@ -1,3 +1,35 @@
 /**
 	古くなったお知らせを削除するバッチ
 */
+global.db_path = './db/mockbu.db';
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(global.db_path);
+var userNotification = require('../models/user_notification');
+
+
+console.log('start');
+
+// 開始前の全体数
+userNotification.countObjects(null, function (err, cnt) {
+
+	console.log('before count: ', cnt);
+
+
+	// 削除処理
+	var now = new Date().getTime();
+	var targetTimestamp = now - 3 * 24 * 60 * 60 * 1000;  // 3日前
+	var sql = 'delete from user_notification where create_at <= ' + targetTimestamp;
+	db.run(sql, function (err) {
+
+
+		// 処理後の全体数
+		userNotification.countObjects(null, function (err, cnt) {
+
+			console.log('after count: ', cnt);
+			console.log('finish');
+		});
+
+	});
+
+});
+
