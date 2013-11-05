65bc59db1a795cd318430202014495d6660918c7
Yohei Munesada <y.munesada@gmail.com>
2013-11-06 00:44:43 +0900

    未ログインでPOPが投稿できるバグを修正

--- routes/pop.js
+++ routes/pop.js
@@ -94,7 +94,7 @@ exports.add = function(req, res) {
 
     // uidからユーザー情報を取得する
     var uid = req.cookies.uid;
-    var user_id = (global.sessionMap ? global.sessionMap[uid] : undefined);
+    var user_id = (uid && global.sessionMap ? global.sessionMap[uid] : undefined);
     if (!user_id) {
         res.json(403, {message: 'authentication error.'});
         return;
