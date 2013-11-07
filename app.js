1470afc33211e25454982686beafdfe7a587962e
Yohei.Munesada <munesada_yohei@cyberagent.co.jp>
2013-11-07 10:20:44 +0900

    modif app

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ global.log4js = require('log4js');
 
 
 
+
 // load settings.
 global.log4js.configure('settings/log4js_setting.json');
 var json = fs.readFileSync('./settings/setting.json', 'utf-8');
