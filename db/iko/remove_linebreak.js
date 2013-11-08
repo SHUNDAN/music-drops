362da91bde9f5ec72f89e8bebd0526d4cca9e260
Yohei.Munesada <munesada_yohei@cyberagent.co.jp>
2013-11-08 17:07:07 +0900

    modif bug

--- db/iko/remove_linebreak.js
+++ db/iko/remove_linebreak.js
@@ -1,22 +1,14 @@
 /**
     POPの改行を削除する
 */
-var fs = require('fs');
-
-
-// mysql setting.
-var json = fs.readFileSync('./settings/setting.json', 'utf-8');
-global.mbSetting = JSON.parse(json);
-var mysql = require('mysql');
-global.dbConnectionPool = mysql.createPool(global.mbSetting.mysql);
-
-var popModel = require('../../models/pop');
-
-
+global.db_path = './db/mockbu.db';
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(global.db_path);
 
 // 全件取得する
 var pops;
-popModel.selectObjects(null, function (err, rows) {
+
+db.all('select id, comment from pop', function (err, rows) {
     console.log('target count = ', rows.length);
 
     pops = rows;
@@ -37,9 +29,16 @@ var trimLineBreak = function () {
     var newComment = pop.comment.replace(/\n/g, '');
     if (pop.comment !== newComment) {
 
-        popModel.updateObject({comment:newComment}, {id:pop.id}, function () {
+        var sql = 'update pop set comment="'+newComment+'" where id=' + pop.id;
+        db.run(sql, function (err) {
+
+            if (err) {
+                return process.exit(1);
+            }
+
             console.log('remove line break. id=', pop.id);
             trimLineBreak();
+
         });
 
 
